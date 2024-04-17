26cd242ec80b803ace1552eb44949c4293416c4b
Max Fischer <maxfischer072@gmail.com>
2024-04-17 15:48:10 -0700

    Changed styling

--- portfolio/src/components/AboutSection.js
+++ portfolio/src/components/AboutSection.js
@@ -18,17 +18,17 @@ export class About extends Component {
                         <h3>This site is a collection of some of my best work.</h3><br/>
                         <div className="about_links">
                             <Card className="about_link_card" title={<label className="about_link_card_title">Links</label>}>
-                                <IoLogoLinkedin />
-                                <a href="https://www.linkedin.com/in/max-fischer72/">LinkedIn </a>
-                                <br/>
-                                <IoLogoGithub />
-                                <a href="https://github.com/maxFischer42">Github </a>
-                                <br/>
-                                <a href="https://monadokami.itch.io">Itch.io</a>
-                                <br/>
-                                <a href="https://docs.google.com/document/d/1Jvi1ZUb_PqNzOiD7BCuycFscLG6u_ZKt/edit?usp=sharing&ouid=104650230237681519173&rtpof=true&sd=true">Resume</a>
-                                <br/>
-                                <a href="https://docs.google.com/document/d/1OXkFU_Graw45ha01WAM6k9p5sSl14nRNoXgyhVRjYKI/edit?usp=sharing">Resume/CV</a>                            
+                                <div className="about_link_style">
+                                    <a href="https://www.linkedin.com/in/max-fischer72/"><IoLogoLinkedin /> LinkedIn </a>
+                                    <br/>                                
+                                    <a href="https://github.com/maxFischer42"> <IoLogoGithub /> Github </a>
+                                    <br/>
+                                    <a href="https://monadokami.itch.io">Itch.io</a>
+                                    <br/>
+                                    <a href="https://docs.google.com/document/d/1Jvi1ZUb_PqNzOiD7BCuycFscLG6u_ZKt/edit?usp=sharing&ouid=104650230237681519173&rtpof=true&sd=true">Resume</a>
+                                    <br/>
+                                    <a href="https://docs.google.com/document/d/1OXkFU_Graw45ha01WAM6k9p5sSl14nRNoXgyhVRjYKI/edit?usp=sharing">Resume/CV</a>         
+                                </div>                   
                             </Card>
                         </div>
                         <br/>
